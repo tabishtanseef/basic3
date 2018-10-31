85af8dab91f4c5f4deebde889c06b7811908fdae
Tabish Tanseef <tabishtanseef@gmail.com>
2018-10-31 15:22:04 +0530

    third done

---
+++ Activity-1/js/data.js
@@ -0,0 +1,33 @@
+var heading = "Drag the words";
+var helpTxt = "Next from the words given below each sentence drag the noun and the adjective that describes it to the correct columns.";
+var activitTitle = "Drag the words.";
+var msgComplete = "End of Activity";
+var btnLabel = {
+	"submitbtn": "submit",
+	"nextbtn" : "SHOW Answer",
+	"tryAgain": "Try Again"
+}
+
+
+var dataCollection = [
+	"<span class='draggable'>Karan</span> and <span class='draggable'>Sudhir</span> went to the <span class='draggable'>National Museum</span>.",
+	"I have read the <span class='draggable'>book</span> <span class='draggable'>The Adventure of Huckleberry Finn</span> by <span class='draggable'>Mark Twain</span>.",
+	"<span class='draggable'>Cheena</span> went for her <span class='draggable'>holidays</span>.",
+	"The <span class='draggable'>teacher</span> praised <span class='draggable'>Mehul</span> for his <span class='draggable'>honesty</span>.",
+	"<span class='draggable'>Mrs Reena</span> bought a <span class='draggable'>silk</span> from <span class='draggable'>Mysore</span>.",
+	"We should wear <span class='draggable'>clothes</span> made of <span class='draggable'>cotton</span> when it is hot.",
+	"<span class='draggable'>Chanchal</span> is proud of her <span class='draggable'>beauty</span>.",
+	"While building a <span class='draggable'>house</span>, we use <span class='draggable'>cement</span>, <span class='draggable'>sand</span>, <span class='draggable'>concrete</span>.",
+	"The <span class='draggable'>flowers</span> have a very nice <span class='draggable'>fragrance</span>.",
+	"Everyone admires the old <span class='draggable'>woman</span> for her <span class='draggable'>grace</span> and <span class='draggable'>elegance</span>."
+]
+
+var softTh = ["Karan","Sudhir","National Museum","The Adventure of Huckleberry Finn","Mark Twain","Cheena","Dubai","Chanchal","Mehul","Mrs Reena","Mysore"];
+var hardTh = ["book","holidays","teacher","clothes","house","flowers","woman"];
+var tTh=["silk","cotton","cement","sand","concrete"];
+var dTh=["honesty","beauty","fragrance","grace","elegance"];
+
+
+
+
+
